b077dd4f62bb09708f3bbb555e6a57cb14a83b61
Samuel Tardieu <sam@rfc1149.net>
2022-01-18 15:38:33 +0100

    Make xmlsec feature compile

--- bindings.h
+++ bindings.h
@@ -4,6 +4,8 @@
 #include <xmlsec/app.h>
 #include <xmlsec/crypto.h>
 #include <xmlsec/keys.h>
+#include <xmlsec/openssl/app.h>
+#include <xmlsec/openssl/crypto.h>
 #include <xmlsec/templates.h>
 #include <xmlsec/transforms.h>
 #include <xmlsec/xmldsig.h>
